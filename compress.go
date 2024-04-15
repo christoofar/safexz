bb872718bfa223c3371a5d0a2881de13ae62bb53
christoofar <sawyer.christopher@protonmail.com>
2024-04-15 16:44:35 -0400

    layout the Go interface

--- compress.go
+++ compress.go
@@ -1,4 +1,9 @@
+// Description: This file contains the implementation of the compression functions.  These
+// functions are used to compress data using the xz format.  You don't need to worry about
+// the C language bindings, as they are handled in the lzma package.
 package safexz
+
+import "io"
 
 func CompressString(s string) (string, error) {
 	return "", nil
@@ -6,4 +11,20 @@ func CompressString(s string) (string, error) {
 
 func CompressBytes(b []byte) ([]byte, error) {
 	return nil, nil
+}
+
+func CompressFile(path string) error {
+	return nil
+}
+
+func CompressFileWithProgress(path string, progress func(float64)) error {
+	return nil
+}
+
+func CompressFileToMemory(path string) ([]byte, error) {
+	return nil, nil
+}
+
+func CompressStream(input io.Reader, output io.Writer) error {
+	return nil
 }
--- decompress.go
+++ decompress.go
@@ -1,4 +1,9 @@
+// Description: This file contains the implementation of the decompression functions for safexz.
+// These functions are used to decompress data that has been compressed using the xz format.
+// You do not need to worry about the C language bindings, as they are handled in the lzma package.
 package safexz
+
+import "io"
 
 func DecompressString(compressedString string) (string, error) {
 	return "", nil
@@ -6,4 +11,20 @@ func DecompressString(compressedString string) (string, error) {
 
 func DecompressBytes(compressedBytes []byte) ([]byte, error) {
 	return nil, nil
+}
+
+func DecompressFile(input_path, output_path string) error {
+	return nil
+}
+
+func DecompressFileWithProgress(input_path, output_path string, progress func(float64)) error {
+	return nil
+}
+
+func DecompressFileToMemory(path string) ([]byte, error) {
+	return nil, nil
+}
+
+func DecompressStream(input io.Reader, output io.Writer) error {
+	return nil
 }
